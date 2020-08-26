a4e8bdf7046155aba89362542f750ca788646e2b
Dmitriy.Panov <Dmitriy.Panov@jetbrains.com>
2020-08-26 23:41:01 +0300

    MRESOLVER-134 Unsatisfiable Range header causing 416 HTTP error reproduced

--- maven-resolver-demos/maven-resolver-demo-snippets/src/main/java/org/apache/maven/resolver/examples/ResolveArtifact.java
+++ maven-resolver-demos/maven-resolver-demo-snippets/src/main/java/org/apache/maven/resolver/examples/ResolveArtifact.java
@@ -8,9 +8,9 @@ package org.apache.maven.resolver.examples;
  * to you under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance
  * with the License.  You may obtain a copy of the License at
- * 
+ *
  *  http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
@@ -26,6 +26,10 @@ import org.eclipse.aether.artifact.Artifact;
 import org.eclipse.aether.artifact.DefaultArtifact;
 import org.eclipse.aether.resolution.ArtifactRequest;
 import org.eclipse.aether.resolution.ArtifactResult;
+
+import java.io.IOException;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
 
 /**
  * Resolves a single artifact.
@@ -47,8 +51,26 @@ public class ResolveArtifact
         RepositorySystem system = Booter.newRepositorySystem();
 
         RepositorySystemSession session = Booter.newRepositorySystemSession( system );
+        Path localRepo = session.getLocalRepository().getBasedir().toPath();
+        if (Files.exists(localRepo)) Files.walkFileTree(localRepo, new SimpleFileVisitor<Path>() {
+            @Override
+            public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+                Files.delete(file);
+                return FileVisitResult.CONTINUE;
+            }
 
-        Artifact artifact = new DefaultArtifact( "org.eclipse.aether:aether-util:1.0.0.v20140518" );
+            @Override
+            public FileVisitResult postVisitDirectory(Path dir, IOException exc) throws IOException {
+                Files.delete(dir);
+                return FileVisitResult.CONTINUE;
+            }
+        });
+        Path brokenJarPartSource = Paths.get("maven-resolver-demos/maven-resolver-demo-snippets/src/main/resources/junit-jupiter-api-5.0.0.jar.part");
+        Path brokenJarPartTarget = localRepo.resolve("org/junit/jupiter/junit-jupiter-api/5.0.0/junit-jupiter-api-5.0.0.jar.part");
+        Files.createDirectories(brokenJarPartTarget.getParent());
+        Files.copy(brokenJarPartSource, brokenJarPartTarget);
+
+        Artifact artifact = new DefaultArtifact( "org.junit.jupiter:junit-jupiter-api:5.0.0" );
 
         ArtifactRequest artifactRequest = new ArtifactRequest();
         artifactRequest.setArtifact( artifact );
